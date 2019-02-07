201061903698b68cc963ed57678d79e77a335ccd
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-07 04:16:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,11 +7,16 @@ def initialize(name)
   @backed_projects = []
   @name = name  
   @project = project   
-  Project.add_backer(self)
 end
 
 def back_project(project)
   self.backed_projects << project
+  Project.add_backer(self)
 end
 
 end
+
+def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
